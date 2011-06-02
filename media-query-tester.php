c8e461e27bc4aa187134dacbf0d7412173972c35
Frank Bültge <frank@bueltge.de>
2011-06-02 22:16:20 +0200

    add form me ribbon

--- media-query-tester.php
+++ media-query-tester.php
@@ -32,6 +32,8 @@
 	
 </head> 
 <body>
+	<a href="https://github.com/bueltge/Simple-Media-Queries-Tester"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://d3nwyuy0nl342s.cloudfront.net/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub"></a>
+	
 	<header>
 		<h1><a href="http://bueltge.de/" title="to the weblog of the author">Simple Media Queries Tester</a></h1>
 	</header>
